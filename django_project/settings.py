c77f7116b7f109119afdb72c47a9850ed3417dc6
popo26 <mynameispopo26@gmail.com>
2022-09-19 15:24:05 +1200

    f

--- django_project/settings.py
+++ django_project/settings.py
@@ -200,8 +200,8 @@ SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 #Heroku
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # STATICFILES_DIRS = (
 # os.path.join(BASE_DIR, 'static'),

aa33373690acc8e5d88133308c46ccc21e3457ad
popo26 <mynameispopo26@gmail.com>
2022-09-22 15:35:22 +1200

    test

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,8 @@ SECRET_KEY =os.environ["DJANGO_SECRET_KEY"] or "secret"
 
 
 # #Heroku
-DEBUG = os.environ.get('DJANGO_DEBUG', False)
+# DEBUG = os.environ.get('DJANGO_DEBUG', False)
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['whatsmyagendatoday.herokuapp.com', '127.0.0.1']

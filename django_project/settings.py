a56507dba84fbf22999f42d4a0a6014c2c549992
popo26 <mynameispopo26@gmail.com>
2022-09-23 06:19:21 +1200

    test

--- django_project/settings.py
+++ django_project/settings.py
@@ -172,7 +172,7 @@ SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 #Heroku
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 STATICFILES_DIRS = (
